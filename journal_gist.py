87559528370a2fff12f778845e2c47abe671766d
Evan Woods <22337004+efwoods@users.noreply.github.com>
2024-07-01 21:54:16 -0400

    created initial test

---
+++ journal_gist.py
@@ -0,0 +1,34 @@
+"""This module will summarize a research paper"""
+
+# Imports
+import logging
+from dotenv import load_dotenv
+from openai import OpenAI
+
+logging.basicConfig(level=logging.DEBUG, filename='journal_gist_output.log')
+
+LOAD_ENV_SUCCESS =  load_dotenv()
+if not LOAD_ENV_SUCCESS:
+    # logic to pull env from github
+    logging.error("Error: No Environment Variable Set")
+
+def query_openai(messages: list) -> list:
+    """This function is used to query the OpenAI API.
+
+    Args:
+        messages (list): This is the list of messages to send to the API.
+
+    Returns:
+        list: This is the response to the message queries.
+    """
+    client = OpenAI()
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+
+    return completion
+
+if __name__ == "__main__":
+    pass
--- tests/test_journal_gist.py
+++ tests/test_journal_gist.py
@@ -5,11 +5,13 @@ import logging
 import sys
 import os
 
-logging.basicConfig(level=logging.DEBUG, 
+logging.basicConfig(level=logging.DEBUG,
                     filename="test_journal_gist_output.log")
 
-sys.path.insert(0, 
+sys.path.insert(0,
                 os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
+
+import journal_gist
 
 class TestJournalGist(unittest.TestCase):
     """This class is used to define specific unit tests.
@@ -18,13 +20,20 @@ class TestJournalGist(unittest.TestCase):
         unittest (module): This module is used to enable unit testing.
     """
 
-    def setUp():
+    def setUp(self) -> None:
         pass
 
-    def tearDown():
+    def tearDown(self) -> None:
         pass
 
-
+    def test_openai_messaging_functions(self) -> None:
+        """This method is used to test basic messaging functions."""
+        messages = [
+            {"role": "system", "content":"You are a helpful assistant."},
+            {"role":"user", "content":"Hello!"}
+        ]
+        response = journal_gist.query_openai(messages)
+        logging.info(response)
 
 if __name__ == '__main__':
     unittest.main()
